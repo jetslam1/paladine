d1efb66b197b68789bb48407e58795d4c76245c3
beleko <idgamerz@web.de>
2010-01-06 14:30:37 +0100

    [57] Added Enum for Malygos
    Signed-off-by: beleko <idgamerz@web.de>

--- src/bindings/Scriptdev2/system/ScriptLoader.cpp
+++ src/bindings/Scriptdev2/system/ScriptLoader.cpp
@@ -290,6 +290,7 @@ extern void AddSC_boss_keristrasza();
 extern void AddSC_boss_ormorok();
 extern void AddSC_boss_telestra();
 extern void AddSC_instance_nexus();
+extern void AddSC_boss_malygos();                           // Eye of Eternity
 extern void AddSC_boss_sartharion();                        //obsidian_sanctum
 extern void AddSC_instance_obsidian_sanctum();
 extern void AddSC_instance_vault_of_archavon();             //vault_of_archavon
@@ -722,6 +723,7 @@ void AddScripts()
     AddSC_boss_ormorok();
     AddSC_boss_telestra();
     AddSC_instance_nexus();
+	AddSC_boss_malygos();                                   // Eye of Eternity
     AddSC_boss_sartharion();                                //obsidian_sanctum
     AddSC_instance_obsidian_sanctum();
 	AddSC_instance_vault_of_archavon();                     //vault_of_archavon
