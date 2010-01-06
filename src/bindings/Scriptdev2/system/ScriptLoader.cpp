dab632a06bd2ff28be3d406b660c4b126f894934
beleko <idgamerz@web.de>
2010-01-06 17:41:59 +0100

    [58] Fix Linux Compille and Delate Malygos Script
    Signed-off-by: beleko <idgamerz@web.de>

--- src/bindings/Scriptdev2/system/ScriptLoader.cpp
+++ src/bindings/Scriptdev2/system/ScriptLoader.cpp
@@ -290,7 +290,6 @@ extern void AddSC_boss_keristrasza();
 extern void AddSC_boss_ormorok();
 extern void AddSC_boss_telestra();
 extern void AddSC_instance_nexus();
-extern void AddSC_boss_malygos();                           // Eye of Eternity
 extern void AddSC_boss_sartharion();                        //obsidian_sanctum
 extern void AddSC_instance_obsidian_sanctum();
 extern void AddSC_instance_vault_of_archavon();             //vault_of_archavon
@@ -723,7 +722,6 @@ void AddScripts()
     AddSC_boss_ormorok();
     AddSC_boss_telestra();
     AddSC_instance_nexus();
-	AddSC_boss_malygos();                                   // Eye of Eternity
     AddSC_boss_sartharion();                                //obsidian_sanctum
     AddSC_instance_obsidian_sanctum();
 	AddSC_instance_vault_of_archavon();                     //vault_of_archavon
