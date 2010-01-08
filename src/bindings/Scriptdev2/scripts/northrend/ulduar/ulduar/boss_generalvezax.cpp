6197103a42a3c3b1c8bd007da14cf07eccb87db3
beleko <idgamerz@web.de>
2010-01-08 17:18:17 +0100

    [76] General Vezax Script beginning !
    Signed-off-by: beleko <idgamerz@web.de>

--- src/bindings/Scriptdev2/scripts/northrend/ulduar/ulduar/boss_generalvezax.cpp
+++ src/bindings/Scriptdev2/scripts/northrend/ulduar/ulduar/boss_generalvezax.cpp
@@ -16,7 +16,7 @@
 
 /* ScriptData
 SDName: General Vezax
-SD%Complete: 0
+SD%Complete: 10%
 SDComment: PH.
 SDCategory: Ulduar
 EndScriptData */
@@ -24,6 +24,28 @@ EndScriptData */
 #include "precompiled.h"
 #include "ulduar.h"
 
+// General Vezax Spells
+#define SPELL_BERSERK              26662
+#define SPELL_AURA_OF DESPAIR      62692
+#define SPELL_MARK_OF_THE FACELESS 63276
+#define SPELL_SARONITE_BARRIER     63364
+#define SPELL_SEARING_FLAMES       62661
+#define SPELL_SHADOW_CRASH         62660
+#define SPELL_SURGE_OF_DARKNESS    62662
+
+// General Vezax Sounds
+#define SOUND_UR_Vezax_Aggro01     15542
+#define SOUND_UR_Vezax_Attack      15550
+#define SOUND_UR_Vezax_Berserk01   15547
+#define SOUND_UR_Vezax_Death01     15546
+#define SOUND_UR_Vezax_HardMode01  15548
+#define SOUND_UR_Vezax_Kite01      15545
+#define SOUND_UR_Vezax_Slay01      15543
+#define SOUND_UR_Vezax_Slay02      15544
+#define SOUND_UR_Vezax_Wound       15549
+#define SOUND_UR_Vezax_WoundCrit   15551
+
+// General Vezax Says
 /*
 #define SAY_AGGRO -1
 #define SAY_SLAY -1
@@ -53,7 +75,7 @@ struct MANGOS_DLL_DECL boss_generalvezaxAI : public ScriptedAI
 
     void Aggro(Unit* pWho)
     {
-//        DoScriptText(SAY_AGGRO, m_creature);
+        DoScriptText(SOUND_UR_Vezax_Aggro01, m_creature);
         m_creature->SetInCombatWithZone();
 
         if (m_pInstance)
@@ -89,4 +111,4 @@ void AddSC_boss_vezax()
     newscript->RegisterSelf();
 
 }
-
+/*UPDATE `creature_template` SET `ScriptName`='boss_generalvezax' WHERE (`entry`='33271') LIMIT 1*/
