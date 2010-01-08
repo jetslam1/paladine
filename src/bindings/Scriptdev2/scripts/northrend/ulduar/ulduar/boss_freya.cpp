01c1c569457f27c40239e14e07eb77d64c99b15c
beleko <idgamerz@web.de>
2010-01-08 21:56:53 +0100

    [86] Ulduar Freya Update
    Signed-off-by: beleko <idgamerz@web.de>

--- src/bindings/Scriptdev2/scripts/northrend/ulduar/ulduar/boss_freya.cpp
+++ src/bindings/Scriptdev2/scripts/northrend/ulduar/ulduar/boss_freya.cpp
@@ -24,6 +24,8 @@ EndScriptData */
 #include "precompiled.h"
 #include "ulduar.h"
 
+#define GOSSIP_START     "Port to The Descent into Madness ?"
+
 /*
 #define SAY_AGGRO -1
 #define SAY_SLAY -1
@@ -80,13 +82,128 @@ CreatureAI* GetAI_boss_freya(Creature* pCreature)
     return new boss_freyaAI(pCreature);
 }
 
+struct MANGOS_DLL_DECL boss_freya_helpAI : public ScriptedAI
+{
+    boss_freya_helpAI(Creature* pCreature) : ScriptedAI(pCreature)
+    {
+        m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
+        Reset();
+    }
+
+    ScriptedInstance* m_pInstance;
+
+    void Reset()
+    {
+    }
+
+    void KilledUnit(Unit *victim)
+    {
+    }
+
+    void JustDied(Unit *victim)
+    {
+    }
+
+    void Aggro(Unit* pWho)
+    {
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
+            return;
+
+        DoMeleeAttackIfReady();
+
+        EnterEvadeIfOutOfCombatArea(diff);
+
+    }
+
+};
+
+CreatureAI* GetAI_boss_freya_help(Creature* pCreature)
+{
+    return new boss_freya_helpAI(pCreature);
+}
+
+struct MANGOS_DLL_DECL boss_freya_copyAI : public ScriptedAI
+{
+    boss_freya_copyAI(Creature* pCreature) : ScriptedAI(pCreature)
+    {
+        m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
+        Reset();
+    }
+
+    ScriptedInstance* m_pInstance;
+
+    void Reset()
+    {
+    }
+
+    void BeginFreyaCopyEvent(Player* pPlayer)
+    {
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        DoMeleeAttackIfReady();
+
+        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim()) return;
+
+        if (!m_pInstance)
+            return;
+    }
+};
+
+CreatureAI* GetAI_boss_freya_copy(Creature* pCreature)
+{
+    return new boss_freya_copyAI(pCreature);
+}
+
+bool GossipHello_boss_freya_copy(Player* pPlayer, Creature* pCreature)
+{
+    pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_START, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
+
+    pPlayer->SEND_GOSSIP_MENU(pPlayer->GetGossipTextId(pCreature), pCreature->GetGUID());
+    return true;
+}
+
+bool GossipSelect_boss_freya_copy(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
+{
+    if (uiAction == GOSSIP_ACTION_INFO_DEF+1)
+    {
+        pPlayer->CLOSE_GOSSIP_MENU();
+        ((boss_freya_copyAI*)pCreature->AI())->BeginFreyaCopyEvent(pPlayer);
+    }
+
+    return true;
+}
+
 void AddSC_boss_freya()
 {
-    Script *newscript;
-    newscript = new Script;
-    newscript->Name = "boss_freya";
-    newscript->GetAI = &GetAI_boss_freya;
-    newscript->RegisterSelf();
+    Script* NewScript;
+
+    NewScript = new Script;
+    NewScript->Name = "boss_freya";
+    NewScript->GetAI = &GetAI_boss_freya;
+    NewScript->RegisterSelf();
+
+	NewScript = new Script;
+    NewScript->Name = "boss_freya_help";
+    NewScript->GetAI = &GetAI_boss_freya_help;
+    NewScript->RegisterSelf();
+
+	NewScript = new Script;
+    NewScript->Name = "boss_freya_copy";
+    NewScript->GetAI = &GetAI_boss_freya_copy;
+    NewScript->pGossipHello = &GossipHello_boss_freya_copy;
+    NewScript->pGossipSelect = &GossipSelect_boss_freya_copy;
+    NewScript->RegisterSelf();
 
 }
+
+/* UPDATE `creature_template` SET `ScriptName`='boss_freya' WHERE (`entry`='32906');
+   UPDATE `creature_template` SET `ScriptName`='boss_freya_copy' WHERE (`entry`='33241');
+   UPDATE `creature_template` SET `ScriptName`='boss_freya_help' WHERE (`entry`='33410')
+   UPDATE `creature_template` SET `npcflag`='1' WHERE (`entry`='33241'); */
 
