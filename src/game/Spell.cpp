f9776e2d7cb5a30d3031e5707614506652e22660
beleko <idgamerz@web.de>
2010-01-10 14:02:49 +0100

    [116] Fix Error
    Signed-off-by: beleko <idgamerz@web.de>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2224,7 +2224,7 @@ void Spell::SetTargetMap(uint32 effIndex, uint32 targetMode, UnitList& targetUni
             // FOR EVERY TARGET TYPE THERE IS A DIFFERENT FILL!!
             if (m_spellInfo->SpellFamilyFlags2 & UI64LIT (0x00000020) && m_spellInfo->SpellIconID == 3217)
             {
-                TagUnitMap.push_back(m_caster);
+                targetUnitMap.push_back(m_caster);
                 break;
             }
             switch(m_spellInfo->Effect[effIndex])
