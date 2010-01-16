68fd4c1898ffda2baf5b9998e9d220272aa2c15c
beleko <idgamerz@web.de>
2010-01-16 11:39:21 +0100

    [188] Fixed build error at *nix.
    Signed-off-by: beleko <idgamerz@web.de>

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -299,6 +299,7 @@ class Item;
 class Pet;
 class Path;
 class PetAura;
+class Totem;
 class Vehicle;
 
 struct SpellImmune
@@ -1267,6 +1268,8 @@ class MANGOS_DLL_SPEC Unit : public WorldObject
 
         Pet* CreateTamedPetFrom(Creature* creatureTarget,uint32 spell_id = 0);
 
+        Totem* GetTotem(uint8 slot) const;
+
         template<typename Func>
         void CallForAllControlledUnits(Func const& func, bool withTotems, bool withGuardians, bool withCharms);
         template<typename Func>
@@ -1681,6 +1684,8 @@ class MANGOS_DLL_SPEC Unit : public WorldObject
         float GetCombatRatingReduction(CombatRating cr) const;
         uint32 GetCombatRatingDamageReduction(CombatRating cr, float rate, float cap, uint32 damage) const;
 
+        Unit* _GetTotem(uint8 slot) const;                  // for templated function without include need
+
         uint32 m_state;                                     // Even derived shouldn't modify
         uint32 m_CombatTimer;
 
@@ -1717,9 +1722,8 @@ void Unit::CallForAllControlledUnits(Func const& func, bool withTotems, bool wit
     if (withTotems)
     {
         for (int8 i = 0; i < MAX_TOTEM; ++i)
-            if(m_TotemSlot[i])
-                if(Creature *totem = GetMap()->GetCreature(m_TotemSlot[i]))
-                    func(totem);
+            if (Unit *totem = _GetTotem(i))
+                func(totem);
     }
 
     if (withCharms)
@@ -1747,10 +1751,9 @@ bool Unit::CheckAllControlledUnits(Func const& func, bool withTotems, bool withG
     if (withTotems)
     {
         for (int8 i = 0; i < MAX_TOTEM; ++i)
-            if (m_TotemSlot[i])
-                if (Creature *totem = GetMap()->GetCreature(m_TotemSlot[i]))
-                    if (func(totem))
-                        return true;
+            if (Unit *totem = _GetTotem(i))
+                if (func(totem))
+                    return true;
     }
 
     if (withCharms)
